6a8d1bdb625899b975aa4b5afddaf6569944f6ff
Qyss <mywh1t3h4nd@gmail.com>
2023-08-07 22:07:33 +0900

    초기 Views 계획

--- views.py
+++ views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+def login(request):
+    pass
+
+def dashboard(request):
+    pass
+
+def asset_manage(request, editing):
+    pass
+
+def host_manage(request, editing):
+    pass
+
+def asset_details(request, editing):
+    pass
+
+def history(request):
+    pass
+
+def rack_diagram(reqeust, editing):
+    pass
