42b23ce82d10ed801f55c33457e164d83f6197d0
Qyss <mywh1t3h4nd@gmail.com>
2023-08-07 22:14:56 +0900

    Admin에 필요 Model 추가

--- admin.py
+++ admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import IDC, Rack, EquipGroup, Product, Asset
 
 # Register your models here.
+admin.site.register(IDC)
+admin.site.register(Rack)
+admin.site.register(EquipGroup)
+admin.site.register(Product)
+admin.site.register(Asset)
